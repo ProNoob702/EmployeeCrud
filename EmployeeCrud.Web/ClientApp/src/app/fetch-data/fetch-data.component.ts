ac835a872a53676d7b9b389e8367034c586d7e92
taha.seddik1992@gmail.com <taha.seddik1992@gmail.com>
2022-07-30 00:37:47 +0100

    display employees

--- EmployeeCrud.Web/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ EmployeeCrud.Web/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -3,21 +3,24 @@ import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-fetch-data',
-  templateUrl: './fetch-data.component.html'
+  templateUrl: './fetch-data.component.html',
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[] = [];
+  public employees: IEmployee[] = [];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
+    http.get<IEmployee[]>(baseUrl + 'api/employees').subscribe(
+      (result) => {
+        this.employees = result;
+      },
+      (error) => console.error(error)
+    );
   }
 }
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+interface IEmployee {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
 }
